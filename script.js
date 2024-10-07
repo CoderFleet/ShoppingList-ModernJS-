4cee64fa989a2a8c43218d5712136ad5b66bc36a
Rudransh Pratap Singh <87255169+CoderFleet@users.noreply.github.com>
2024-10-07 19:38:57 +0530

    Added Delete & Clear Functionality with Filter working

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+// Some starter HTML element selectors
+
 const form = document.getElementById("item-form");
 const input = document.getElementById("item-input");
 const button = document.getElementsByClassName("btn");
+const itemList = document.querySelector("#item-list");
+const itemFilter = document.getElementById("filter");
+const clearBtn = document.getElementById("clear");
+
+// Function to add items to shopping list
 
 function addItem(content) {
   const itemList = document.getElementById("item-list");
@@ -20,8 +27,45 @@ function addItem(content) {
   item.appendChild(itemContentBtn);
 
   itemList.appendChild(item);
+
+  checkUI();
 }
 
+function deleteItem(e) {
+  if (e.target.parentElement.classList.contains("remove-item")) {
+    if (confirm("Are You Sure?")) {
+      e.target.parentElement.parentElement.remove();
+      checkUI();
+    }
+  }
+}
+
+function checkUI() {
+  const items = itemList.querySelectorAll("li");
+  if (items.length === 0) {
+    clearBtn.style.display = "none";
+    itemFilter.style.display = "none";
+  } else {
+    clearBtn.style.display = "block";
+    itemFilter.style.display = "block";
+  }
+}
+
+function filterItems(e) {
+  const items = itemList.querySelectorAll("li");
+  const enteredText = e.target.value.toLowerCase();
+
+  items.forEach((item) => {
+    const itemName = item.firstChild.textContent.toLowerCase();
+    if (itemName.indexOf(enteredText) != -1) {
+      item.style.display = "flex";
+    } else {
+      item.style.display = "none";
+    }
+  });
+}
+
+// Form Submit Event Listener
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -32,3 +76,19 @@ form.addEventListener("submit", (e) => {
   }
   input.value = "";
 });
+
+// Delete Button & Clear Functionality logic
+itemList.addEventListener("click", (e) => deleteItem(e));
+
+clearBtn.addEventListener("click", () => {
+  while (itemList.firstChild) {
+    itemList.firstChild.remove();
+  }
+  checkUI();
+});
+
+// Item filtering functionality ==>
+itemFilter.addEventListener("input", (e) => filterItems(e));
+
+// On document load check for empty list
+checkUI();
